5be80a213ca8bd47331d95e619db38a6263cb9be
Strushko Danila <danya.strushko@mail.ru>
2024-03-26 14:26:51 +0300

    #10.1 - create service for user

---
+++ src/main/java/com/practice/diplom/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.practice.diplom.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String e){
+        super(e);
+    }
+}
---
+++ src/main/java/com/practice/diplom/service/UserService.java
@@ -0,0 +1,18 @@
+package com.practice.diplom.service;
+
+import com.practice.diplom.dto.UserRequestDto;
+import com.practice.diplom.dto.UserResponseDto;
+import com.practice.diplom.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<UserResponseDto> getAllUsers();
+    void updateUser(UserRequestDto newUser, Long id);
+
+    void deleteUser(Long id);
+
+    User getUserById(Long id);
+
+    UserResponseDto getUserDtoById(Long id);
+}
